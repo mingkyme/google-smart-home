b77b0b119a693440118bd43db4156664d05e40f9
MINGKYME <admin@mingky.me>
2021-08-21 17:52:00 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+// Import the appropriate service
+const { smarthome } = require('actions-on-google')
+
+// Create an app instance
+const app = smarthome()
+// Register handlers for Smart Home intents
+app.onSync((body, headers) => {
+    return {
+      requestId: body.requestId,
+      payload: {
+        agentUserId: 123,
+        devices: []
+      },
+    }
+  })
+  app.onQuery((body, headers) => {
+    return {
+      requestId: 'ff36...',
+      payload: {
+        // ...
+      },
+    }
+  })
+app.onExecute((body, headers) => {
+  return {
+    requestId: 'ff36...',
+    payload: {
+      // ...
+    },
+  }
+})
+app.onDisconnect((body, headers) => {
+  return {}
+})
+
+const express = require('express');
+const expressApp = express().use(express.json())
+expressApp.listen(3000)
+expressApp.post('/fulfillment', app)
